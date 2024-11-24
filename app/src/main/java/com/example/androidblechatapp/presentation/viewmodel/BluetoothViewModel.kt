45f3ca4295e31ef0f98970c08400c8c2e9aab142
Aakash Chaturvedi <aakashch@mail.uni-paderborn.de>
2024-11-24 17:11:52 +0100

    set up VM

---
+++ app/src/main/java/com/example/androidblechatapp/presentation/viewmodel/BluetoothViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.androidblechatapp.presentation.viewmodel
+
+import androidx.compose.runtime.MutableState
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.androidblechatapp.domain.BluetoothController
+import com.example.androidblechatapp.presentation.BluetoothUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+
+@HiltViewModel
+class BluetoothViewModel(
+    private val bluetoothController: BluetoothController
+): ViewModel() {
+
+    private val _uiState = MutableStateFlow(BluetoothUiState())
+
+    val state = combine(
+        bluetoothController.pairedDevice,
+        bluetoothController.scannedDevice,
+        _uiState
+    ) { scannedDevices,  pairedDevices , state ->
+        state.copy(
+            pairedDevices = pairedDevices,
+            scannedDevices = scannedDevices
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), _uiState.value)
+
+    fun startScan() = bluetoothController.startDiscovery()
+
+    fun stopScan() = bluetoothController.stopDiscovery()
+
+
+}
